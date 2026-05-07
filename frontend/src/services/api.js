cff1dee6df7063869db1d62174ee6425ef3da79d
manasvjain <manasvjainmj@gmail.com>
2026-05-07 23:59:11 +0530

    Connected live backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000"
+baseURL: "https://microdoppler-ai-dashboard.onrender.com"
+
+
 });
 
 export default API;
