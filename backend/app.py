05a4c6a20ab34d0d04ccb18af6ff164b810595a0
manasvjain <manasvjainmj@gmail.com>
2026-05-07 23:30:44 +0530

    Fixed Render deployment

--- backend/app.py
+++ backend/app.py
@@ -40,3 +40,16 @@ async def predict(file: UploadFile = File(...)):
         "prediction": result["class"],
         "confidence": result["confidence"]
     }
+
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+
+    uvicorn.run(
+        "app:app",
+        host="0.0.0.0",
+        port=port
+    )
+
